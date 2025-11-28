d463a04dc0831c92fee091cc3706f82d09b987ec
Aaron Fuenzalida <afuenzalida243@gmail.com>
2025-11-28 18:46:35 -0300

    fix middleware

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import attendance 
 
 app = FastAPI(
     title="API CrossChex Integration",
     description="API para consultar horas trabajadas",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # "*" permite que todos se puedan conectar
+    allow_credentials=True,
+    allow_methods=["*"],  # permite todos los metodos (GET, POST, OPTIONS, etc.)
+    allow_headers=["*"],  # permite todos los encabezados
 )
 
 app.include_router(attendance.router)
